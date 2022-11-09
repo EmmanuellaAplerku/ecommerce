81d2f2b490b998667a8db08a5258a798d3ac5da5
EmmanuellaAplerku <94686032+EmmanuellaAplerku@users.noreply.github.com>
2022-11-09 17:19:20 +0000

    index

--- index.php
+++ index.php
@@ -6,30 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
 	
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link href="index.css" rel="stylesheet" type="text/css">
-
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
     <link href='https://fonts.googleapis.com/css?family=Raleway:400, 600' rel='stylesheet' type='text/css'>
-  <link rel='stylesheet' href='style.css'/> -->
+  <link rel='stylesheet' href='style.css'/> 
   </head>
   
   <body>
+
   <style>
 	body {
   background-image: url('https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvcHBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60');
 	}
 	</style>
- <!-- <section class="section-one">  -->
-    <div class="container">
+
+<nav>
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">Revamp'D</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="./view/all_product.php">All Products</a></li>
+          <li><a href="./Login/login.php">Login</a></li> 
+				
+						<li>
+						<form action="../actions/product_functions.php" method= 'get'>
+							<input type="search" name="search" id="search" style="width:100px">
+							<button name= "submit">search</button>
+						</form>
+            <li><a href="./Login/register.php">SignUp</a></li> 
+						</li>
+					
+          
+			</div>
+</nav>
+
+<section class="section-one"> 
+<div class="container">
       <h1>Welcome to Revamp'd!</h1>
       <p>Shopping at your door step</p>
-      <a href="Login/register.php" class="home-button">Create an Account</a>
+      <!-- <a href="Login/register.php" class="home-button">Create an Account</a>
       </br>
       <a href="Login/login.php" class="home-button">I already have an account</a>
       </br>
-      <a href="view/all_product.php" class="home-button">See Products Available</a>
+      <a href="view/all_product.php" class="home-button">See Products Available</a> -->
       </br>
       </br>
       </br>
@@ -44,20 +67,6 @@
       </br>
       </br>
       </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
-
 
 
       <p>Admin login details:</p>
@@ -66,8 +75,8 @@
 
 
 	  
-    </div>  
-  <!-- </section>  -->
+</div>  
+  </section> 
  
  
   
--- view/product_search_result.php
+++ view/product_search_result.php
@@ -3,8 +3,10 @@
     include ("../controllers/product_controller.php");
    
 	if(isset($_POST['searchtitle'])){
+		// echo "it works";
 		$term=$_POST['search'];
 		$item=search_product($term);
+		// print_r($item);
 	}
    
 ?>
@@ -33,22 +35,27 @@
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
 					<?php
 					
-					 if (check_login_index()== false){?>
-						<li><a href="../Login/login.php">Login</a></li>
-					<?php } elseif( $_SESSION['role']== 1){ 
-					
+					if(check_login()==false){
+						header('location:../index.php');
+					}
+
+					else{
+						?>
+						<li><a href="../Login/logout.php" class="home-button">Logout</a></li>
+						<?php
+
+					}
+					if(isset($_SESSION['role'])==1){
 					?>
-						
-						<li><a href="Login/logout.php">Logout</a></li>
-						<li><a href="admin/brand.php">Brand</a></li>
-						<li><a href="admin/category.php">Category</a></li>
-						<li><a href="admin/product.php">Add Product</a></li>
+
+					<li><a href="all_product.php">All Products</a></li>
 
 					<?php 
-					}else{
+
+					}
+					else{
 					?>
-						<li><a href="../Login/logout.php">Logout</a></li>
-						<li><a href="all_product.php">All Products</a></li>
+						
 						<li>
 						<form action="../actions/product_functions.php" method= 'get'>
 							<input type="search" name="search" id="search" style="width:100px">
@@ -56,13 +63,66 @@
 						</form>
 						</li>
 					<?php }?>
-						<li><a href="../Login/register.php">SignUp</a></li>
+						<!-- <li><a href="../Login/register.php">SignUp</a></li> -->
 				</ul>
 			</div>
 		</nav>
         
-    
-		<form method="POST">
+		<?php
+		foreach ($item as $things){ 
+			?>
+			<div class="product-content product-wrap clearfix">
+						<div class="row">
+							<div class="col-md-5 col-sm-12 col-xs-12">
+								<div>
+									<img id="img1" width="300" height="300" style='border-radius: 8px' src="<?php echo $things['product_image']; ?>" alt="" srcset="">
+								</div>
+							</div>
+							<div class="col-md-7 col-sm-12 col-xs-12">
+								<div class="product-deatil">
+									<h5 class="name">
+										<div>
+											<?php
+											echo 'Title:';
+											echo $things['product_title'];
+											?>
+										</div>
+
+									</h5>
+									<div>
+										<?php
+										echo '$';
+										echo $things['product_price'];
+										?>
+									</div>
+									<div>
+										<?php
+										echo 'Description:  ';
+										echo $things['product_desc'];
+										?>
+									</div>
+								</div>
+								<div class="product-info smart-form">
+									<div class="row">
+										<div class="col-md-6 col-sm-6 col-xs-6">
+											<a href="javascript:void(0);" class="btn btn-success">Add to cart</a>
+											<?php 
+											echo "
+											<a href='../view/single_product.php?product_id={$things['product_id']}' class='btn btn-success'>View product</a>
+											";
+											?>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			
+					<?php
+				}
+		?>
+		<form method="POST" action="">
         <h1>Search</h1>
 		<label>Search:</label><br>
 		<input type="text" name="search">
