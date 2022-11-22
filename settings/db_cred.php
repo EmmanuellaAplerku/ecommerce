2ad26dca5f545af1cc23ad19d142411f641d74f2
EmmanuellaAplerku <94686032+EmmanuellaAplerku@users.noreply.github.com>
2022-11-22 00:12:06 +0000

    fd

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,16 +1,16 @@
 <?php
 // // //Database credentials
-    define('SERVERNAME','localhost');
-    define('HOSTNAME','root');
-    define('PASSWORD','');
-    define('DATABASE','shoppn');
+    // define('SERVERNAME','localhost');
+    // define('HOSTNAME','root');
+    // define('PASSWORD','');
+    // define('DATABASE','shoppn');
 
 
-// //Database credentials
-// define('SERVERNAME','us-cdbr-east-06.cleardb.net');
-// define('HOSTNAME','b2c356ae70efdf');
-// define('PASSWORD','254aa936');
-// define('DATABASE','heroku_0711bd41e53453d');
+//Database credentials
+define('SERVERNAME','us-cdbr-east-06.cleardb.net');
+define('HOSTNAME','b2c356ae70efdf');
+define('PASSWORD','254aa936');
+define('DATABASE','heroku_0711bd41e53453d');
 
 
 ?>
