630f33067adbaafe3cd94b18a2598d147cebaa35
EmmanuellaAplerku <94686032+EmmanuellaAplerku@users.noreply.github.com>
2022-11-24 13:54:12 +0000

    search

--- view/product_search_result.php
+++ view/product_search_result.php
@@ -41,9 +41,9 @@
 
 					else{
 						?>
-						<li><a href="../index.php" class="home-button">Home</a></li>
+						<li><a href="all_product.php" class="home-button">See Products</a></li>
 						<li><a href="cart.php" class="home-button">View Cart</a></li>
-						<li><a href="product_search_result.php" class="home-button">Search</a></li>
+						
 						<li><a href="../Login/logout.php" class="home-button">Logout</a></li>
 						<?php
 
